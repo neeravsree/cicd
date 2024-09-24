eaea9f25a561e9a296df94a3f00561a8682acf17
sree <sree.sreelekshmi@gmail.com>
2024-09-24 05:34:05 +0530

    initial commit

---
+++ src/main/java/com/cicddemo/controller/CiCdController.java
@@ -0,0 +1,14 @@
+package com.cicddemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CiCdController {
+
+	
+	@GetMapping("/welcome")
+	public String getMessage() {
+		return "Welcome";
+	}
+}
